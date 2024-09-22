ff8d1023b182f1ab2b824c84f0d03ad5176d59d9
DylanW22 <dylan.whitehouse22@gmail.com>
2024-09-22 11:15:42 +0800

    Created .env file and added environment variables

--- src/utilities/firebaseConfig.ts
+++ src/utilities/firebaseConfig.ts
@@ -6,14 +6,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "_REDACTED",
-  authDomain: "_REDACTED",
-  projectId: "_REDACTED",
-  storageBucket: "_REDACTED.appspot.com",
-  messagingSenderId: "_REDACTED",
-  appId: "1:_REDACTED:web:_REDACTED",
-  measurementId: "_REDACTED",
-  databaseURL: "https://_REDACTED-default-rtdb.firebaseio.com",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
