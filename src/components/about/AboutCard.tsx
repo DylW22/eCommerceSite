54d5a013fff75165e5f7315f31802942dbc9fe04
DylanW22 <dylan.whitehouse22@gmail.com>
2024-09-27 15:34:11 +0800

    Fixed theme issue

--- src/components/about/AboutCard.tsx
+++ src/components/about/AboutCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import downArrow from "../../assets/down-arrow.svg";
+import rightArrow from "../../assets/right-arrow.svg";
 interface AboutCardProps {
   title: string;
   content: string;
@@ -13,11 +14,24 @@ export const AboutCard: React.FC<AboutCardProps> = ({ title, content }) => {
   };
   return (
     <div
-      style={{ cursor: "pointer" }}
-      onClick={toggleOpen}
-      className={`mb-3 border border-secondary bg-white p-3 rounded flex-fill d-flex flex-column justify-content-center align-items-center`}
+      style={{ cursor: "pointer", minHeight: isOpen ? "150px" : "50px" }}
+      onClick={toggleOpen} //d-flex flex-column justify-content-center align-items-center
+      className={`mb-3 border border-secondary bg-white p-3 rounded flex-fill text-midnight position-relative`}
     >
-      <h3>{title}</h3>
+      <h3 className="text-center">{title}</h3>
+      <div className="position-absolute" style={{ top: "10%", right: "5%" }}>
+        <img
+          style={{ height: "20px" }}
+          className=""
+          src={isOpen ? rightArrow : downArrow}
+        />
+      </div>
+      {/*<p
+        className="position-absolute top-0 fs-1 fw-bold"
+        style={{ right: "2%" }}
+      >
+        {`${isOpen ? "Down" : ">"}`}
+      </p>*/}
       <div
         style={{
           height: isOpen ? "auto" : "0",
